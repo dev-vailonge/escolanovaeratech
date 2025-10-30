5c66442c0c7d8011d65ee09b185c697fb68f980f
dev-vailonge <devvailonge@gmail.com>
2025-10-30 12:20:13 +0000

    add script tk page

--- src/app/black/thank-you/page.tsx
+++ src/app/black/thank-you/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Script from 'next/script'
 import { motion } from 'framer-motion'
 import { useState, useEffect } from 'react'
 
@@ -15,6 +16,30 @@ export default function BlackThankYouPage() {
   }, [])
 
   return (
+<>
+    <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+
+fbq('init', '394878869825377');
+fbq('track', 'PageView');
+
+// Dispara o evento de Lead
+fbq('track', 'Lead');
+</script>
+
+<noscript>
+  <img height="1" width="1" style="display:none"
+  src="https://www.facebook.com/tr?id=394878869825377&ev=PageView&noscript=1"/>
+</noscript>
+<!-- End Meta Pixel Code -->
     <main className="min-h-screen bg-black text-white flex items-center justify-center relative">
       {/* Background gradient */}
       <div className="absolute inset-0 bg-gradient-to-br from-black via-black to-yellow-500/10"></div>
@@ -102,5 +127,6 @@ export default function BlackThankYouPage() {
         </motion.div>
       </div>
     </main>
+        </>
   )
 }
