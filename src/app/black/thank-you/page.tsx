d7ced15ffab32f416bd91a550429a7086742f2c4
dev-vailonge <devvailonge@gmail.com>
2025-10-30 12:41:37 +0000

    Update page.tsx

--- src/app/black/thank-you/page.tsx
+++ src/app/black/thank-you/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Script from 'next/script'
 import { motion } from 'framer-motion'
 import { useState, useEffect } from 'react'
 
@@ -15,6 +16,35 @@ export default function BlackThankYouPage() {
   }, [])
 
   return (
+
+    <>
+     <>
+      <Script id="facebook-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          
+          fbq('init', '394878869825377');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=394878869825377&ev=PageView&noscript=1"
+          alt="facebook pixel"
+        />
+      </noscript>
+
     <main className="min-h-screen bg-black text-white flex items-center justify-center relative">
       {/* Background gradient */}
       <div className="absolute inset-0 bg-gradient-to-br from-black via-black to-yellow-500/10"></div>
@@ -102,5 +132,6 @@ export default function BlackThankYouPage() {
         </motion.div>
       </div>
     </main>
+    </>
   )
 }
